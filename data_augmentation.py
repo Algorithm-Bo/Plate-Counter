6f7a5dc1552f689feab53b970306edddb664f80e
Algorithm-Bo <44469212+Algorithm-Bo@users.noreply.github.com>
2021-01-01 20:29:22 +0100

    Update

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import imgaug as ia
 import imgaug.augmenters as iaa
 import os
 import random
@@ -12,6 +13,8 @@ IMG_HEIGHT = 256
 IMG_CHANNELS = 3
 
 TRAIN_PATH = 'train_data_256x256_without_augmentation_special_new'
+
+output_foldername = 'augmented_images_imgaug_5'
 
 train_ids = next(os.walk(TRAIN_PATH))[1]
 
@@ -34,14 +37,10 @@ for n in range(0, len(train_ids)):
 
 images = X_train
 segmaps = np.array(Y_train * 255, dtype=np.uint16)
-"""
-seq = iaa.Sequential([
-    iaa.GaussianBlur((0, 3.0)),
-    iaa.Affine(translate_px={"x": (-40, 40)}),
-    iaa.Crop(px=(0, 10))
-])
-"""
-aug2 = iaa.SomeOf(1, [ # 1 zufällige auswählen
+
+ia.seed(42)
+
+aug1 = iaa.SomeOf(1, [ # 1 zufällige auswählen
     iaa.GaussianBlur((0, 3.0)), # Blur (unscharf)
     iaa.Fliplr(0.5), # flip horizontally
     iaa.Flipud(0.5), # flip vertically
@@ -61,42 +60,55 @@ aug2 = iaa.SomeOf(1, [ # 1 zufällige auswählen
     iaa.Crop(px=(0, 10))
 ])
 
-"""
-images_aug, segmaps_aug = seq(images=images, segmentation_maps=segmaps)
+# image = images[0]
+# segmap = segmaps[0]
 
-seq.show_grid([images_aug[0], images_aug[1]], cols=8, rows=8)
+# Augment images and segmaps.
+images_aug = []
+segmaps_aug = []
+num_augmentations = 16
 
-seq.show_grid([segmaps_aug[0], segmaps_aug[1]], cols=8, rows=8)
-"""
-# num_original_samples = 43
-# j = num_original_samples + 1
+for n in range(0, len(images)):
+    image = images[n]
+    segmap = segmaps[n]
+    print('segmap.shape: ', segmap.shape)
+    segmap = np.expand_dims(segmap, axis=0)
+    print('segmap.shape: ', segmap.shape)
+    for _ in range(num_augmentations):
+        images_aug_i, segmaps_aug_i = aug1(image=image, segmentation_maps=segmap)
+        images_aug.append(images_aug_i)
+        segmaps_aug.append(segmaps_aug_i)
 
-images_aug, segmaps_aug = aug2(images=images, segmentation_maps=segmaps)
+# print('images_aug.shape: ', images_aug.shape)
+# print('segmaps_aug.shape: ', segmaps_aug.shape)
 
-# noch ändern:
+# images_aug, segmaps_aug = aug1(images=images, segmentation_maps=segmaps)
+# images_aug2 = np.array([aug1.augment_image(images[1]) for _ in range(16)])
+
+# samples addieren
 """
 for i in range (1, 5):
 
-    images_aug_new, segmaps_aug_new = aug2(images=images, segmentation_maps=segmaps)
+    images_aug_new, segmaps_aug_new = aug1(images=images, segmentation_maps=segmaps)
     print('Hallo')
     images_aug = images_aug + images_aug_new
     segmaps_aug = segmaps_aug + segmaps_aug_new
-    # aug2.show_grid([images_aug[0], images_aug[1]], cols=8, rows=8)
+    # aug1.show_grid([images_aug[0], images_aug[1]], cols=8, rows=8)
     print(images_aug.shape)
 """
-aug2.show_grid([images_aug[0], images_aug[1]], cols=8, rows=8)
-# aug2.show_grid([segmaps_aug[0], segmaps_aug[1]], cols=8, rows=8)
+
+# img_grid = aug1.show_grid([images_aug[0], images_aug[1]], cols=8, rows=8)
+# aug1.show_grid([segmaps_aug[0], segmaps_aug[1]], cols=8, rows=8)
 
 # save augmented images and masks to folders
-
 counter = 0
-save_as_counter = 602 # augmented data ab save_as_counter.PNG
+save_as_counter = 100 # augmented data ab save_as_counter.PNG
+
 for elem in images_aug:
     img_augmented = np.squeeze(images_aug[counter])
+    
     mask_augmented = np.squeeze(segmaps_aug[counter])
-    # print(img_augmented.shape)
-
-    output_foldername = 'augmented_images_imgaug_3_new'
+    print(img_augmented.shape)
 
     # Ausgabeordner erstellen, falls nicht vorhanden
     if not os.path.isdir(output_foldername):
@@ -112,59 +124,6 @@ for elem in images_aug:
     imsave(img_path, img_augmented)
     imsave(mask_path, mask_augmented * 255)
 
-    # cv.imwrite(img_path, img_augmented)
-    # cv.imwrite(mask_path, mask_augmented_grayscale * 255)
-
     counter += 1
     save_as_counter += 1
 
-# example
-"""
-
-import numpy as np
-import imgaug as ia
-import imgaug.augmenters as iaa
-
-
-ia.seed(1)
-
-# Example batch of images.
-# The array has shape (32, 64, 64, 3) and dtype uint8.
-images = np.array(
-    [ia.quokka(size=(64, 64)) for _ in range(32)],
-    dtype=np.uint8
-)
-
-seq = iaa.Sequential([
-    iaa.Fliplr(0.5), # horizontal flips
-    iaa.Crop(percent=(0, 0.1)), # random crops
-    # Small gaussian blur with random sigma between 0 and 0.5.
-    # But we only blur about 50% of all images.
-    iaa.Sometimes(
-        0.5,
-        iaa.GaussianBlur(sigma=(0, 0.5))
-    ),
-    # Strengthen or weaken the contrast in each image.
-    iaa.LinearContrast((0.75, 1.5)),
-    # Add gaussian noise.
-    # For 50% of all images, we sample the noise once per pixel.
-    # For the other 50% of all images, we sample the noise per pixel AND
-    # channel. This can change the color (not only brightness) of the
-    # pixels.
-    iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
-    # Make some images brighter and some darker.
-    # In 20% of all cases, we sample the multiplier once per channel,
-    # which can end up changing the color of the images.
-    iaa.Multiply((0.8, 1.2), per_channel=0.2),
-    # Apply affine transformations to each image.
-    # Scale/zoom them, translate/move them, rotate them and shear them.
-    iaa.Affine(
-        scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
-        translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
-        rotate=(-25, 25),
-        shear=(-8, 8)
-    )
-], random_order=True) # apply augmenters in random order
-
-images_aug = seq(images=images)
-"""
