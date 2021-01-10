8d97537e7fc08ce1b85d764b68ba3c7e26929ac3
Algorithm-Bo <44469212+Algorithm-Bo@users.noreply.github.com>
2021-01-10 15:12:53 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Plate-Counter
+Modifiziertes U-Net zur semantischen Segmentierung
+Voraussetzung: Trainingsbilder und dazugehörende Annotationsmasken
+
+Programmiersprache Python.
+Die installierten Bibliotheken befinden sich in der Datei „requirements.txt“
+
+Bilder und Masken in entsprechende Ordner „images_train“ (Bilder) bzw. „binary_masks“ (Annotationsmasken) kopieren
+Skript „make_trainfolders.py“ aufrufen (Erzeugt Ordnerstruktur mit Trainingsdatensatz)
+
+Augmentierte Bilder erzeugen mit Skript data_augmentation.py
+Namen von ab Nummer nach Originalbilder.PNG vergeben
+Orginaltrainingsbilder und Masken zu den Unterordnern „img“ und „masks“ hinzufügen
+Skript „make_trainfolders.py“ aufrufen (Erzeugt Ordnerstruktur inklusive augmentierter Bildern)
+
+Ordner der Originalbilder und augmentierten Bilder in gemeinsamen Ordner kopieren (Trainingsdatensatz fürs FCN)
+
+Testbilder (dürfen nicht in den Trainingsbildern vorhanden sein) in den Testbildordner „images_test“ kopieren
+Skript „make_testfolders.py“ aufrufen (Erzeugt Ordnerstruktur mit Testbildern)
+
+Modell trainieren mit Skript „train_model.py“ (Modell wird gespeichert unter „my_model.h5“)
+Oder Vorhersagen mit vortrainiertem Modell „predict.py“ (lädt „my_model.h5)
+Ausgabe der Ergebnisbilder (Binärbilder) jeweils in Ordner „results“
+
+Auswerten der Ergebnisbilder (Zählen+Positionen) mit Skript „evaluate.py“
+Ausgabe der Zählergebnisse des Testdatensatzes in CSV Datei „results.CSV“ in Ordner „result_lists_CSV“
+Ausgabe der Ergebnisse (Objektpositionen im Einzelbild) in JSON Dateien „Nummer_Testbild.JSON“ im Ordner „JSON_counted_objects“
--- make_prediction_512.py
+++ make_prediction_512.py
@@ -8,20 +8,38 @@ from skimage.transform import resize
 import matplotlib.pyplot as plt
 import cv2 as cv
 
-IMG_WIDTH = 256
-IMG_HEIGHT = 256
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+
+IMG_WIDTH = 512
+IMG_HEIGHT = 512
 IMG_CHANNELS = 3
-img_size = (IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS)
 
-# initialize
+# configure folders
+# TEST_PATH = 'test_data_staphylococcus_512x512'
+TEST_PATH = 'images_test_beispiele'
 
-TEST_PATH = 'test_data_pseudomonas_512x512'
+output_foldername = 'results_val_split_0_25_batchsize_8_imgaug_staphylococcus_512x512_final_modified_beispiele'
 
-output_foldername = 'results_val_split_0_1_batchsize_8_imgaug_3_special_new_pseudomonas_512x512'
+# Free up RAM
+tf.keras.backend.clear_session()
 
+# load pretrained model
+model = load_model('my_model_512_modified.h5')
+model.summary()
+
+# load test images
 test_ids = next(os.walk(TEST_PATH))[1]
 
-# test images
 X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
 sizes_test = []
 print('Load test images') 
@@ -34,43 +52,37 @@ for test_img in range(0, len(test_ids)):
     X_test[n] = img
     n += 1
 
-# load pretrained model
-model = load_model('my_model.h5')
-
-model.summary()
-
-# predict
+# make predictions
 preds_test = model.predict(X_test, verbose=1)
 
-# make binary (aufrunden auf 1)
-preds_test_t = (preds_test > 0.5).astype(np.uint8)
+# make binary
+threshold = 0.5
+preds_test_t = (preds_test > threshold).astype(np.uint8)
 
 # save test images
 counter = 0
-save_as_counter = 0 # 90 # Testdata ab Bild 90.PNG
+
+# filenames starts with save_as_counter.PNG
+save_as_counter = 0
+
 for result in preds_test:
     img_squeezed = np.squeeze(preds_test[counter])
     img_squeezed_t = np.squeeze(preds_test_t[counter])
-    img_squeezed_test_original = np.squeeze(X_test[counter])
-    
-    # Ausgabeordner erstellen, falls nicht vorhanden
+        
+    # create test folder, if not existing
     if not os.path.isdir(output_foldername):
         os.mkdir(output_foldername)
-    if not os.path.isdir(output_foldername+ '/results'):
+    if not os.path.isdir(output_foldername + '/results'):
         os.mkdir(output_foldername + '/results')
-    if not os.path.isdir(output_foldername+ '/results_t'):
+    if not os.path.isdir(output_foldername + '/results_t'):
         os.mkdir(output_foldername + '/results_t')
-    if not os.path.isdir(output_foldername+ '/results_test_original'):
-        os.mkdir(output_foldername + '/results_test_original')
-
+    
     result_path = output_foldername + '/results/' + str(save_as_counter) + '.PNG'
     result_path_t = output_foldername + '/results_t/' + str(save_as_counter) + '.PNG'
-    result_path_test_original = output_foldername + '/results_test_original/' + str(save_as_counter) + '.PNG'
-
+   
     cv.imwrite(result_path, img_squeezed * 255)
     cv.imwrite(result_path_t, img_squeezed_t * 255)
-    cv.imwrite(result_path_test_original, img_squeezed_test_original)
-    
+   
     counter += 1
     save_as_counter += 1
 
